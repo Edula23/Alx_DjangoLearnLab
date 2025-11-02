e7fb9a7e2917b53be2624606b1af824e128fdc6c
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-11-02 18:58:59 +0300

    Update

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -11,4 +11,4 @@ admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
      list_display = ("title", "author", "published_year")
      search_fields = ("title", "author")
-     list_filter = ("published_year",)
+     list_filter = ("publication_year",)
