4cbe5525c7aaad02fd149d0f60498b2e671f4ee8
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-07 21:28:41 +0300

    Modify comment upadate and create

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -20,7 +20,17 @@ class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "blog/register.html"
-class CommentCreateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class CommentCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = "blog/comment_form.html"
