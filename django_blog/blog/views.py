7817cbacc45ba04152adfa49ff87a7926f2735ad
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-07 21:39:31 +0300

    Add search functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -102,7 +102,7 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
-    ordering = ["-published_date"]
+    ordering = ["-published_date", "Post.objects.filter", "title__icontains", "tags__name__icontains", "content__icontains"]
 
 class PostDetailView(DetailView):
     model = Post
