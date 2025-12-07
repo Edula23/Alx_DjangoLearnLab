071ee385124d4d10cce9f841740dab5a32266662
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-07 19:01:49 +0300

    Add urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.views.generic import TemplateView 
-from blog.views import SignUpView
+from blog.views import Register
 urlpatterns = [
-    path('login/', TemplateView.as_view(template_name='accounts/login.html'), name='login'),
-    path('register/', TemplateView.as_view(template_name='accounts/register.html'), name='register'),
-    path("profile/", SignUpView.as_view(), name="templates/registration/profile."),
-]
+    path('login/', TemplateView.as_view(template_name='templates/blog/registration/login.html'), name='login'), 
+    path("profile/", TemplateView.as_view(), template_name="templates/accounts/profile.html", name='profile' ),
+    path('register/', Register.as_view(template_name='templates/blog/registration/register.html'), name='register'), ]
+   
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
-class SignUpView(CreateView):
+class Register(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    success_url = reverse_lazy("register")
+    template_name = "templates/blog/registration/register.html"
 # Create your views here.
