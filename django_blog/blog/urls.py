9aaf2f3142c346ac00aa43450c0ab50cc699a836
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-07 21:11:58 +0300

    Update urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,10 @@ from blog.views import (
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
     path("posts/", PostListView.as_view(), name="post_list"),
-    path("posts/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
     path("login/", TemplateView.as_view(template_name="blog/login.html"), name="login"),
     path("register/", Register.as_view(), name="register"),
     path("profile/", user_profile_view, name="profile"),
