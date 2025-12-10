653d0b35b707523860a23f5a30d2a8cfa90b1363
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-10 23:28:55 +0300

    modify query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,6 +3,8 @@ from models import Library
 from models import Librarian
 
 books_by_author = Book.objects.filter(author='John Doe')
-booksInLibrary = Library.objects.all()
-librarianInLibrary = Librarian.objects.filter(library = 'lib')
+library = Library.objects.get(name="Central Library")
+bookInLIbrary = library.books.all()
+librarianInLibrary = library.librarian
+
 
