a364d717646ac1b81f01dbb47f4e85ca978415fa
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-11 00:02:40 +0300

    modify html name

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,8 @@ from django.views.generic import DetailView
 # Create your views here.
 def listBooks(request):
     books = Book.objects.all()
-    context = {'book_list': books}
-    return render(request, 'books/book_list.html', context)
+    context = {'list_books': books}
+    return render(request, 'books/list_books.html', context)
 class libraryDetailView(DetailView):
     model = Library
     template_name = 'books/library_detail.html'
