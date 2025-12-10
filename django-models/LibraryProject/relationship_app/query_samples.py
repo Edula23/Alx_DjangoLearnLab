590bb322a8538a1abfacaac29aefcc26afef9490
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-10 23:53:27 +0300

    add views for list books and library view

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,6 +2,8 @@ from models import Book
 from models import Library
 from models import Librarian
 from models import Author
+author_name = "Eden"
+library_name = "Digital"
 
 author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Book
+from .models import Library
+from django.views.generic import DetailView
 # Create your views here.
+def listBooks(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'books/book_list.html', context)
+class libraryDetailView(DetailView):
+    model = Library
+    template_name = 'books/library_detail.html'
