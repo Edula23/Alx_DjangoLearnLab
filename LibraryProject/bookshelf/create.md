6aa2025fe28cd69ec393732129207ff3a73efa91
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-10 22:57:14 +0300

    add models for relationship app

--- LibraryProject/bookshelf/create.md
+++
@@ -1,3 +0,0 @@
-from bookshelf.models import Book
-book = Book.objects.create(title="1984", author="George Orwell", published_year=1949)
-# created new book instance
--- LibraryProject/relationship_app/query_samples.py
+++
@@ -1,3 +0,0 @@
-books = Book.objects.all() 
-books_by_library = Book.objects.filter(library='John Doe')
-librarian_by_library = Librarian.objects.filter(library='John Doe')
